f5c2949bdf7ca8c9fbd176eaac7755d82859b07b
agm54c <agm54c@gmail.com>
2025-11-22 00:23:33 +0800

    能跑SFT

--- TALE-PT.py
+++ TALE-PT.py
@@ -124,7 +124,7 @@ def inference_eval(sample_list, base_model_path, lora_path, batch_size):
         model=merged_model_path,
         tokenizer=base_model_path,
         gpu_memory_utilization=0.90,  # ✅ 使用 90% 显存
-        max_model_len=512,  # ✅ 降低序列长度
+        max_model_len=4096,  # ✅ 降低序列长度
         enforce_eager=True,  # ✅ 禁用 CUDA graph 节省显存
     )
     tokenizer = AutoTokenizer.from_pretrained(base_model_path, local_files_only=True)
